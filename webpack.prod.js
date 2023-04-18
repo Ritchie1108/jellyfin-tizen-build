26c1cd1ffc851cf9ff6b9903d4c543eac306247b
Ritchie1108 <ritchie1108@gmail.com>
2023-04-18 17:14:27 +0800

    Add files via upload

---
+++ webpack.prod.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    entry: { 'main.jellyfin': './index.jsx' },
+    plugins: [
+        new WorkboxPlugin.InjectManifest({
+            swSrc: path.resolve(__dirname, 'src/serviceworker.js'),
+            swDest: 'serviceworker.js',
+            maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
+        })
+    ]
+});
