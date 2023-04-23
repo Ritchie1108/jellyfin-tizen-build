138f2566df90be7c9ec9fe4f43e606b4fcb2e984
Ritchie1108 <ritchie1108@gmail.com>
2023-04-24 03:12:22 +0800

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,17 +5,16 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    entry: { 'main.jellyfin': './index.jsx' },
+    entry: './scripts/site.js',
     plugins: [
         new WorkboxPlugin.InjectManifest({
             swSrc: path.resolve(__dirname, 'src/serviceworker.js'),
             swDest: 'serviceworker.js',
-            maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
+            maximumFileSizeToCacheInBytes: 4 * 1024 * 1024
         })
     ],
     performance: {
-        hints: false,
-        maxEntrypointSize: 5 * 1024 * 1024,
-        maxAssetSize: 5 * 1024 * 1024
+        maxEntrypointSize: 2 * 1024 * 1024,
+        maxAssetSize: 4 * 1024 * 1024
     }
 });
